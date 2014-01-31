c503afc5a4945975bc04a96ca95dbf4bc650cd43
laurent12 <l.testaert@gmail.com>
2014-01-31 14:05:39 +0100

    first commmit

---
+++ src/converter/Converter.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package converter;
+
+/**
+ * @author admin
+ *
+ */
+public class Converter {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
